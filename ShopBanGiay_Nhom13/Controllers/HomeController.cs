24f8b6d2a5b30efdae05ac4cbaa9d799ddf73509
s1mpnood <109813273+s1mpnood@users.noreply.github.com>
2025-11-12 16:09:18 +0700

    Thêm dashboard, layoutadmin

--- ShopBanGiay_Nhom13/Controllers/HomeController.cs
+++ ShopBanGiay_Nhom13/Controllers/HomeController.cs
@@ -42,5 +42,24 @@ namespace ShopBanGiay_Nhom13.Controllers
             List<SANPHAM> dspk = csdl.SANPHAM.Where(x => x.MAL == x.LOAISP.MAL_CHA).ToList();
             return View(dspk);
         }
+        public ActionResult Dashboard()
+        {
+            var kho = csdl.SANPHAM.Select(s => new Kho
+            {
+                MASP = s.MASP,
+                TENSP = s.TENSP,
+                HINHANH = s.HINHANH,
+                SOLUONG = s.SOLUONG ?? 0,  
+                GIA = s.GIA ?? 0,       
+                MOTA = s.MOTA
+            }).ToList();
+
+            ViewBag.Categories = csdl.LOAISP
+                .Select(l => l.TENL) 
+                .Distinct()
+                .ToList();
+
+            return View(kho);
+        }
     }
 }
