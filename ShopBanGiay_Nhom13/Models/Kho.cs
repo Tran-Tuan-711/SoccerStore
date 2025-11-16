dd66cfc3a875a784f004e1cb57e20c158bfad972
s1mpnood <109813273+s1mpnood@users.noreply.github.com>
2025-11-17 06:28:21 +0700

    Update thêm Models Kho

---
+++ ShopBanGiay_Nhom13/Models/Kho.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopBanGiay_Nhom13.Models
+{
+    public class Kho
+    {
+        public string MASP { get; set; }
+        public string TENSP { get; set; }
+        public string HINHANH { get; set; }
+        public int SOLUONG { get; set; }
+        public decimal GIA { get; set; }
+        public string MOTA { get; set; }
+    }
+}
