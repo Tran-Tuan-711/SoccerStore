010cad6050dd73c21e2dfb0050fd32da1bb09ad9
Tuấn <agtuan2005@gmail.com>
2025-11-17 21:33:55 +0700

    done Thuong Hieu

--- ShopBanGiay_Nhom13/Controllers/HomeController.cs
+++ ShopBanGiay_Nhom13/Controllers/HomeController.cs
@@ -46,53 +46,78 @@ namespace ShopBanGiay_Nhom13.Controllers
         {
             return RedirectToAction("LocSanPham", new { phanLoai = "Giay", maLoai = (string)null });
         }
-        public ActionResult LocSanPham(string MAL)
+        public ActionResult LocSanPham(string MAL,string math)
         {
             ViewBag.lsp = csdl.LOAISP.ToList();
             ViewBag.thuonghieu = csdl.THUONGHIEU.ToList();
 
-
             List<SANPHAM> dssp = csdl.SANPHAM.Where(x => x.LOAISP.MAL == MAL).ToList();
-
-            var loaiSpHienTai = csdl.LOAISP.FirstOrDefault(x => x.MAL == MAL);
-
-
-            string phanLoai;
-            string maCha;
-
-            if (loaiSpHienTai != null)
+            //Loc San Pham Theo Thuong Hieu
+            if (math !=null)
             {
-                maCha = loaiSpHienTai.MAL_CHA;
-                phanLoai = (maCha == null ? "Giay" : "PhuKien");
+
+                List<SANPHAM> dsth = csdl.SANPHAM.Where(x => x.THUONGHIEU.MATH == math).ToList();
+
+                var listMaLoaiLienQuan = dsth.Select(sp => sp.MAL).Distinct().ToList();
+
+                var dsLoaiCon = csdl.LOAISP
+                                      .Where(lsp => listMaLoaiLienQuan.Contains(lsp.MAL))
+                                      .OrderBy(lsp => lsp.MAL_CHA)
+                                      .ToList();
+
+
+                var model = new DanhMucViewModel
+                {
+                    DanhSachSP = dssp,
+                    DanhSachLoaiSP = dsLoaiCon,
+                    PhanLoai = "ThuongHieu",
+                    MaLoaiHienTai = null
+
+                };
+                ViewBag.math = math;
+                return View("DanhMucSanPham", model);
             }
             else
             {
-                phanLoai = "Giay";
-                maCha = null;   
+                var loaiSpHienTai = csdl.LOAISP.FirstOrDefault(x => x.MAL == MAL);
+
+
+                string phanLoai;
+                string maCha;
+
+                if (loaiSpHienTai != null)
+                {
+                    maCha = loaiSpHienTai.MAL_CHA;
+                    phanLoai = (maCha == null ? "Giay" : "PhuKien");
+                }
+                else
+                {
+                    phanLoai = "Giay";
+                    maCha = null;
+                }
+                var dsLoaiCon = new List<LOAISP>();
+
+                if (maCha == null)
+                {
+                    dsLoaiCon = csdl.LOAISP.Where(x => x.MAL_CHA == null).ToList();
+                }
+                else
+                {
+                    dsLoaiCon = csdl.LOAISP.Where(x => x.MAL_CHA == maCha).ToList();
+                }
+                var model = new DanhMucViewModel
+                {
+                    DanhSachSP = dssp,
+                    DanhSachLoaiSP = dsLoaiCon,
+                    PhanLoai = phanLoai,
+                    MaLoaiHienTai = null
+
+                };
+                return View("DanhMucSanPham", model);
             }
-            var dsLoaiCon = new List<LOAISP>();
-
-            if (maCha == null)
-            {
-                dsLoaiCon = csdl.LOAISP.Where(x => x.MAL_CHA == null).ToList();
-            }
-            else
-            {
-                dsLoaiCon = csdl.LOAISP.Where(x => x.MAL_CHA == maCha).ToList();
-            }
-
-
-
-            var model = new DanhMucViewModel
-            {
-                DanhSachSP = dssp,
-                DanhSachLoaiSP = dsLoaiCon,
-                PhanLoai = phanLoai,
-                MaLoaiHienTai = MAL
-            };
-            return View("DanhMucSanPham", model);
         }
-        public ActionResult LocThuongHieu(string math)
+
+        public ActionResult ThuongHieu(string math)
         {
             ViewBag.lsp = csdl.LOAISP.ToList();
             ViewBag.thuonghieu = csdl.THUONGHIEU.ToList();
@@ -112,7 +137,9 @@ namespace ShopBanGiay_Nhom13.Controllers
                 DanhSachLoaiSP = dsLoaiCon,
                 PhanLoai = "ThuongHieu",
                 MaLoaiHienTai = null
+               
             };
+            @ViewBag.math = math;
             return View("DanhMucSanPham", model);
         }
         public ActionResult Giay()
@@ -157,54 +184,54 @@ namespace ShopBanGiay_Nhom13.Controllers
             List<SANPHAM> dsSale = csdl.SANPHAM.Where(sp => sp.MAKM != null).ToList();
             return View(dsSale);
         }
-        public ActionResult Dashboard()
-        {
-            var kho = csdl.SANPHAM.Select(s => new Kho
-            {
-                MASP = s.MASP,
-                TENSP = s.TENSP,
-                HINHANH = s.HINHANH,
-                SOLUONG = s.SOLUONG ?? 0,
-                GIA = s.GIA ?? 0,
-                MOTA = s.MOTA
-            }).ToList();
+        //public ActionResult Dashboard()
+        //{
+        //    var kho = csdl.SANPHAM.Select(s => new Kho
+        //    {
+        //        MASP = s.MASP,
+        //        TENSP = s.TENSP,
+        //        HINHANH = s.HINHANH,
+        //        SOLUONG = s.SOLUONG ?? 0,
+        //        GIA = s.GIA ?? 0,
+        //        MOTA = s.MOTA
+        //    }).ToList();
 
-            ViewBag.Categories = csdl.LOAISP
-                .Select(l => l.TENL)
-                .Distinct()
-                .ToList();
+        //    ViewBag.Categories = csdl.LOAISP
+        //        .Select(l => l.TENL)
+        //        .Distinct()
+        //        .ToList();
 
-            return View(kho);
-        }
-        [HttpPost]
-        public ActionResult Delete(string id)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(id))
-                {
-                    TempData["Error"] = "Không xác định được sản phẩm cần xóa.";
-                    return RedirectToAction("Dashboard");
-                }
+        //    return View(kho);
+        //}
+        //[HttpPost]
+        //public ActionResult Delete(string id)
+        //{
+        //    try
+        //    {
+        //        if (string.IsNullOrEmpty(id))
+        //        {
+        //            TempData["Error"] = "Không xác định được sản phẩm cần xóa.";
+        //            return RedirectToAction("Dashboard");
+        //        }
 
-                var sp = csdl.SANPHAM.FirstOrDefault(x => x.MASP == id);
-                if (sp != null)
-                {
-                    csdl.SANPHAM.Remove(sp);
-                    csdl.SaveChanges();
-                    TempData["Success"] = $"Đã xóa vĩnh viễn sản phẩm: {sp.TENSP}";
-                }
-                else
-                {
-                    TempData["Error"] = "Không tìm thấy sản phẩm cần xóa.";
-                }
-            }
-            catch (Exception ex)
-            {
-                TempData["Error"] = "Lỗi khi xóa sản phẩm: " + ex.Message;
-            }
-            return RedirectToAction("Dashboard");
-        }
+        //        var sp = csdl.SANPHAM.FirstOrDefault(x => x.MASP == id);
+        //        if (sp != null)
+        //        {
+        //            csdl.SANPHAM.Remove(sp);
+        //            csdl.SaveChanges();
+        //            TempData["Success"] = $"Đã xóa vĩnh viễn sản phẩm: {sp.TENSP}";
+        //        }
+        //        else
+        //        {
+        //            TempData["Error"] = "Không tìm thấy sản phẩm cần xóa.";
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["Error"] = "Lỗi khi xóa sản phẩm: " + ex.Message;
+        //    }
+        //    return RedirectToAction("Dashboard");
+        //}
 
     }
 }
